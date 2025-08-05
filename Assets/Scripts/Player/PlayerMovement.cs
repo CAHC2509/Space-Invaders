8a129c2d66fb17398b49f655dba7d26c594e0c18
CAHC2509 <cahc2509.code@gmail.com>
2025-08-05 07:16:00 -0500

    Fix: movement out of limits
    - A bug that allowed players move out of the limits of the map was fixed.

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -24,22 +24,19 @@ public class PlayerMovement : MonoBehaviour, IPlayerComponent
         RemoveListeners();
     }
 
-    private void AddListeners()
-    {
-        PlayerInputReader.OnMovementInput += Move;
-    }
-
-    private void RemoveListeners()
-    {
-        PlayerInputReader.OnMovementInput -= Move;
-    }
+    private void AddListeners() => PlayerInputReader.OnMovementInput += Move;
+    private void RemoveListeners() => PlayerInputReader.OnMovementInput -= Move;
+    private void LateUpdate() => CheckLimits();
 
     private void Move(Vector2 movementInput)
     {
         if (!ActionEnabled) return;
 
         rb.linearVelocityX = movementInput.x * movementSpeed * Time.deltaTime;
+    }
 
+    private void CheckLimits()
+    {
         if (transform.position.x > distanceLimit || transform.position.x < -distanceLimit)
         {
             rb.linearVelocityX = 0f;
