70221937cd1ce45d2b84f044d5bb32f743ca4b6f
CAHC2509 <cahc2509.code@gmail.com>
2025-08-04 20:39:54 -0500

    Feat: default selection on views
    - Every view can define a default button to select every time it's enabled, this allows full time navigation during UI sections.

--- Assets/Scripts/View Base/ViewBase.cs
+++ Assets/Scripts/View Base/ViewBase.cs
@@ -1,12 +1,15 @@
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public abstract class ViewBase : MonoBehaviour
 {
     [SerializeField] protected GameObject viewParent;
 
+    protected GameObject defaultSelection;
+
     public virtual void Initialize() => AddListeners();
     public virtual void Conclude() => RemoveListeners();
-    public virtual void EnableView() => viewParent.SetActive(true);
+    public virtual void EnableView() { viewParent.SetActive(false); EventSystem.current.SetSelectedGameObject(defaultSelection); }
     public virtual void DisableView() => viewParent.SetActive(false);
 
     protected virtual void AddListeners() { }
